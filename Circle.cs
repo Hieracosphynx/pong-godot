b1dcda0cd1dc55b419ec3e24af49a51e1f9d556e
Miguel Cortez <cortezmiguel05@gmail.com>
2024-04-28 14:29:16 -0700

    Initial commit

---
+++ Circle.cs
@@ -0,0 +1,11 @@
+using Godot;
+
+public partial class Circle : Node2D
+{
+    public override void _Draw()
+    {
+		Color white = Colors.White;
+		
+		DrawCircle(Vector2.Zero, 9.8723f, white);
+    }
+}
