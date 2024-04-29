bf84dc8a138c3d6f733309b4a7597e78868e5d1b
Miguel Cortez <cortezmiguel05@gmail.com>
2024-04-28 20:04:13 -0700

    Removed commented code

--- objects/Player.cs
+++ objects/Player.cs
@@ -35,28 +35,5 @@ public partial class Player : CharacterBody2D
 			x: Mathf.Clamp(Position.X, 0, ScreenSize.X),
 			y: Mathf.Clamp(Position.Y, 0, ScreenSize.Y)
 		);
-
-		// Add the gravity.
-		//if (!IsOnFloor())
-			//velocity.Y += gravity * (float)delta;
-
-		//// Handle Jump.
-		//if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
-			//velocity.Y = JumpVelocity;
-
-		// Get the input direction and handle the movement/deceleration.
-		// As good practice, you should replace UI actions with custom gameplay actions.
-		//Vector2 direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
-		//if (direction != Vector2.Zero)
-		//{
-			//velocity.X = direction.X * Speed;
-		//}
-		//else
-		//{
-			//velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
-		//}
-
-		//Velocity = velocity;
-		//MoveAndSlide();
 	}
 }
