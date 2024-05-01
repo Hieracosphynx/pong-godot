5b916329df66f9762a852bfd597746946f35d5c7
Miguel Cortez <cortezmiguel05@gmail.com>
2024-04-30 23:00:24 -0700

    Removed unused functions

--- objects/Ball.cs
+++ objects/Ball.cs
@@ -9,15 +9,6 @@ public partial class Ball : RigidBody2D
 	[Export]
 	public Vector2 velocity = new Vector2(250, 250); 
 
-    public override void _Ready()
-    {
-    }
-
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-	}
-
     public override void _PhysicsProcess(double delta)
     {
 		var collisionInfo = MoveAndCollide(velocity * (float)delta * (float)2.0);
